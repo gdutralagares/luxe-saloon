9d6e6fa256adedd7cb039b40dc463ffa69117a40
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2024-03-21 10:43:53 -0300

    update

--- main.js
+++ main.js
@@ -16,3 +16,9 @@ var swiper = new Swiper(".services-content", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+//header
+let header= document.querySelector('header')
+window.addEventListener('scroll', () =>{
+  header.classList.toggle('shadow', window.scrollY > 0)
+})
