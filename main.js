4abfb2171e8a3a6d8d865a07191d9db063a9b0aa
gabriel dutra lagares <gabriel.lagares20@gmail.com>
2024-03-21 12:00:37 -0300

    update

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let menu =  document.querySelector('.menu-icon')
+let navbar =  document.querySelector('.navbar')
 
 menu.onclick = () =>{
   menu.classList.toggle("move");
+  navbar.classList.toggle("open-menu");
 }
 //swiper
 var swiper = new Swiper(".services-content", {
